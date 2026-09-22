ce7287b5553946065342693a07936577e004638a
tiagozaped@gmail.com <tiagozaped@gmail.com>
2026-09-22 18:58:07 -0300

    feat(navigation): configure tab bar routing, custom ticket active states, and svg assets

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,5 +1,22 @@
 import "@/app/globals.css";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
+
+const TabIcon = () => {
+    <ImageBackground 
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+    >
+    <Image 
+        source={icons.home} 
+        tintColor="#151312" className="size-5"/>
+    <Text 
+        className="text-secondary text-base font-semibold ml-2"
+        >Home
+    </Text>
+    </ImageBackgroud>
+}
 
 export default function _Layout() {
   return (
@@ -9,6 +26,9 @@ export default function _Layout() {
             options={{
                 title: 'Home',
                 headerShown: false
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon focused=({ focused }) icon={icons.home} title="Home" />
+                )
             }}
         
         />
