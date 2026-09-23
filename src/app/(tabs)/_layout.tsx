c26772eddebbcad253bddc2dc8934eb4972c0684
TIAGO ANTUNES PAZ DE OLIVEIRA <tiago.apoliveira1@senacsp.edu.br>
2026-09-23 11:00:32 -0300

    fix(config): update app icon assets and paths in app.json

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -2,42 +2,114 @@ import "@/app/globals.css";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
+import { BlurView } from 'expo-blur';
+import { Image, ImageBackground, Text, View } from "react-native";
 
-const TabIcon = () => {
-    <ImageBackground 
+
+const TabIcon = ({ focused, icon, title } : any) => {
+
+    if (focused) {
+    return (
+    <ImageBackground
         source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
     >
-    <Image 
-        source={icons.home} 
-        tintColor="#151312" className="size-5"/>
-    <Text 
-        className="text-secondary text-base font-semibold ml-2"
-        >Home
-    </Text>
-    </ImageBackgroud>
+        <Image source={icon}
+            tintColor="#151312"
+            className="size-5" 
+        />
+        <Text
+            className="text-secondary text-base font-semibold ml-2"
+        >
+            {title}
+        </Text>
+    </ImageBackground>
+    )}
+
+    return (
+        <View className="size-full justify-center items-center mt-3 rounded-full">
+            <Image 
+                source={icon}
+                tintColor="#A8B5DB"
+                className="size-5"
+            />
+        </View>
+    )
+
 }
+
 
 export default function _Layout() {
   return (
-    <Tabs>
-        <Tabs.Screen 
-            name="index"
-            options={{
-                title: 'Home',
-                headerShown: false
-                tabBarIcon: ({ focused }) => (
-                    <TabIcon focused=({ focused }) icon={icons.home} title="Home" />
-                )
-            }}
-        
-        />
+    <Tabs
+        screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: '100%',
+                height: '100%',
+                justifyContent: 'center',
+                alignItems: 'center'
+
+            },
+
+            tabBarStyle:{
+                backgroundColor: '#1A1A1E',
+                borderRadius: 50,
+                marginHorizontal: 20,
+                marginBottom: 36,
+                height: 52,
+                position: 'absolute',
+                overflow: 'hidden',
+                borderWidth: 1,
+                borderColor: '#0F0D23',
+                // Sombra elegante para dar destaque flutuante
+                shadowColor: '#000',
+                shadowOffset: { width: 0, height: 10 },
+                shadowOpacity: 0.3,
+                shadowRadius: 10,
+            },
+            tabBarBackground: () => (
+                <BlurView 
+                    tint="dark" // Pode usar 'light', 'dark' ou 'default'
+                    intensity={20} // Regula a força do desfoque (0 a 100)
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        bottom: 0,
+                        right: 0,
+                    }}
+                />
+            )
+        }}
+    >
+    <Tabs.Screen 
+        name="index"
+        options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+                <TabIcon 
+                    focused={focused} 
+                    icon={icons.home}
+                    title="Home"
+                />
+            )
+        }}
+    />
 
         <Tabs.Screen 
             name="search"
             options={{
                 title: 'Search',
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon 
+                        focused={focused} 
+                        icon={icons.search}
+                        title="Search"
+                    />
+                )
             }}
         
         />
@@ -46,7 +118,14 @@ export default function _Layout() {
             name="saved"
             options={{
                 title: 'Saved',
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon 
+                        focused={focused} 
+                        icon={icons.save}
+                        title="Saved"
+                    />
+                )
             }}
         
         />
@@ -55,7 +134,14 @@ export default function _Layout() {
             name="profile"
             options={{
                 title: 'Profile',
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon 
+                        focused={focused} 
+                        icon={icons.person}
+                        title="Profile"
+                    />
+                )
             }}
         
         />
